f0233fd2f2ad91007bea4c851b2890cbaa25a2e6
Egor Panevin <predsedatel228@yandex.ru>
2024-03-16 01:42:39 +0300

    add solution for getJSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -214,8 +214,13 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const protoObj = JSON.parse(json);
+  const obj = Object.create(proto);
+  Object.keys(protoObj).forEach((el) => {
+    obj[el] = protoObj[el];
+  });
+  return obj;
 }
 
 /**
